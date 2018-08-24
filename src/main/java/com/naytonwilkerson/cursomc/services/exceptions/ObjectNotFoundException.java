d694a4f14b141e1ef47723b89a248f5685f5389d
NaytonWilkerson <naytonwilkerson78@gmail.com>
2018-08-24 11:56:51 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/naytonwilkerson/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,17 @@
+package com.naytonwilkerson.cursomc.services.exceptions;
+
+import org.springframework.remoting.RemoteTimeoutException;
+
+public class ObjectNotFoundException extends RemoteTimeoutException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	
+	}
+
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg,cause);
+	}
+}
