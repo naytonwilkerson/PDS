559271bfda96033032ff49ee92269c945a51bce5
NaytonWilkerson <naytonwilkerson78@gmail.com>
2018-09-06 19:41:22 -0300

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/naytonwilkerson/cursomc/domain/Cliente.java
+++ src/main/java/com/naytonwilkerson/cursomc/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.naytonwilkerson.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -31,7 +30,6 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
@@ -39,11 +37,13 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	public Cliente() {}
+	public Cliente() {
+		
+	}
 
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
 		super();
