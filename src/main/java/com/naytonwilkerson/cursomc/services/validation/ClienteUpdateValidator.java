d90fee3476dc916749a4370cddb2d24b478a9db7
NaytonWilkerson <naytonwilkerson78@gmail.com>
2018-10-10 20:58:51 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/naytonwilkerson/cursomc/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/naytonwilkerson/cursomc/services/validation/ClienteUpdateValidator.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
